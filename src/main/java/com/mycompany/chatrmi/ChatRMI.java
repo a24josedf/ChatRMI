5cb80db30f49e2d956ab2c8fbc09174f0a566597
a24josedf <dam2_alu04@inf.ald>
2026-02-03 09:00:31 +0100

    primer commit

---
+++ src/main/java/com/mycompany/chatrmi/ChatRMI.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.chatrmi;
+
+/**
+ *
+ * @author dam2_alu04@inf.ald
+ */
+public class ChatRMI {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
